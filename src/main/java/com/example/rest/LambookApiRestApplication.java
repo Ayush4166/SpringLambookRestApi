52ca9f52b85bf85c057e60e19f95735f2d5c8b9c
Ayush4166 <140963331+Ayush4166@users.noreply.github.com>
2024-11-15 23:51:29 +0530

    First commit

---
+++ src/main/java/com/example/rest/LambookApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambookApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambookApiRestApplication.class, args);
+	}
+
+}
